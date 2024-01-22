c81044e79f2f06692c87cf561739f51e966c87dd
Le Nguyen Hung <lnhungtn789@gmail.com>
2024-01-22 15:49:26 +0700

    add repositories (no fuction yet)

---
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/repository/MajorRepository.java
@@ -0,0 +1,10 @@
+package com.usstudy.spring2024se083_usstudy_capstoneproject.domain.repository;
+
+import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.entity.Major;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MajorRepository extends JpaRepository<Major,Integer> {
+
+}
