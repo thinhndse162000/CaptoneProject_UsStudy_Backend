0bfc712bb655db4197e3a84f653523d9ba7d61e5
DESKTOP-AFRRDFT\Admin <thinhndse162000@fpt.edu.vn>
2024-01-25 16:46:00 +0700

    StudentProfile + Customer Account management + Registration form by customer + data sql

---
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/dto/request/CustomerCreatRegistrationFormRequest.java
@@ -0,0 +1,23 @@
+package com.usstudy.spring2024se083_usstudy_capstoneproject.domain.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CustomerCreatRegistrationFormRequest {
+    private String area;
+    private String moreInformation;
+    private String studyAbroadReason;
+    private String destinationReason;
+    private String programChoose;
+    private String majorChoose;
+    private String majorChooseReason;
+    private String universityChooseReason;
+    private String priorityOfStudyAbroad;
+    private String budget;
+}
