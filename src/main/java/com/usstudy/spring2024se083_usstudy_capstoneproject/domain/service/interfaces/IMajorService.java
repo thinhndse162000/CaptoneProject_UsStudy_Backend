713478f035a9333313e4b2b246d1dac08e45ce9f
Le Nguyen Hung <lnhungtn789@gmail.com>
2024-01-26 12:01:37 +0700

    get majors

--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/service/interfaces/IMajorService.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/service/interfaces/IMajorService.java
@@ -1,4 +1,11 @@
 package com.usstudy.spring2024se083_usstudy_capstoneproject.domain.service.interfaces;
 
+import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.entity.Major;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public interface IMajorService {
+    List<Major> getAllMajor();
 }
