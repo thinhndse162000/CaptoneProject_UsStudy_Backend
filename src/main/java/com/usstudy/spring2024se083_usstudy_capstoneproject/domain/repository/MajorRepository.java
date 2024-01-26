d8db7c8b7e3afba7963fbab48e39d19e807ec564
Le Nguyen Hung <lnhungtn789@gmail.com>
2024-01-26 12:01:37 +0700

    get majors

--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/repository/MajorRepository.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/repository/MajorRepository.java
@@ -1,10 +1,14 @@
 package com.usstudy.spring2024se083_usstudy_capstoneproject.domain.repository;
 
 import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.entity.Major;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MajorRepository extends JpaRepository<Major,Integer> {
-
+    List<Major> findAllByOrderByMajorIdDesc();
 }
--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/service/MajorService.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/service/MajorService.java
@@ -1,14 +1,26 @@
 package com.usstudy.spring2024se083_usstudy_capstoneproject.domain.service;
 
+import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.entity.Major;
 import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.repository.MajorRepository;
 import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.service.interfaces.IMajorService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class MajorService implements IMajorService {
     private final MajorRepository majorRepository;
+
+    @Override
+    public List<Major> getAllMajor() {
+        return majorRepository.findAllByOrderByMajorIdDesc();
+    }
 }
