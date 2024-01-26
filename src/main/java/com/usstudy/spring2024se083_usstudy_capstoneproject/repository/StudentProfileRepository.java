76abdaed80f934e5dbcdbfe51350bdd6dcb87ae4
DESKTOP-AFRRDFT\Admin <thinhndse162000@fpt.edu.vn>
2024-01-26 12:54:01 +0700

    Update Config migrate entity

--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/repository/StudentProfileRepository.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/repository/StudentProfileRepository.java
@@ -1,7 +1,6 @@
 package com.usstudy.spring2024se083_usstudy_capstoneproject.repository;
 
 import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.entity.StudentProfile;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StudentProfileRepository extends CrudRepository<StudentProfile, Integer> {
 
-    @Query("SELECT j FROM StudentProfile j "
-            + "WHERE j.CustomerId = :customerId")
+
     Iterable<StudentProfile> findByCustomerId(@Param(value ="req") Integer customerId);
 }
