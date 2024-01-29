0097dea00a525faaf84ad5cc5c5b1667ccb23632
Le Nguyen Hung <lnhungtn789@gmail.com>
2024-01-29 15:46:21 +0700

    View Program type with search id

--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/service/ProgramTypeService.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/service/ProgramTypeService.java
@@ -1,14 +1,28 @@
 package com.usstudy.spring2024se083_usstudy_capstoneproject.domain.service;
 
+import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.entity.ProgramType;
 import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.repository.ProgramTypeRepository;
 import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.service.interfaces.IProgramTypeService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class ProgramTypeService implements IProgramTypeService {
     private final ProgramTypeRepository programTypeRepository;
+
+    @Override
+    public List<ProgramType> getAllProgramType() {
+        return programTypeRepository.findAll();
+    }
+
+    @Override
+    public Optional getProgramTypeById(int id) {
+        return programTypeRepository.findById(id);
+    }
 }
