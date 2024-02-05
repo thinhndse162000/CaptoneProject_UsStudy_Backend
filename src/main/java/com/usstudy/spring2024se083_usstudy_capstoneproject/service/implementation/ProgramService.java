cbbd444b0f1ba305486ce57ad17d4eb7cbe6bd57
Le Nguyen Hung <lnhungtn789@gmail.com>
2024-02-05 08:42:12 +0700

    view program //note: some DB field are missing

--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/service/implementation/ProgramService.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/service/implementation/ProgramService.java
@@ -1,14 +1,28 @@
 package com.usstudy.spring2024se083_usstudy_capstoneproject.service.implementation;
 
+import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.entity.Program;
 import com.usstudy.spring2024se083_usstudy_capstoneproject.repository.ProgramRepository;
 import com.usstudy.spring2024se083_usstudy_capstoneproject.service.IProgramService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class ProgramService implements IProgramService {
     private final ProgramRepository programRepository;
+
+    @Override
+    public List<Program> getAllProgram() {
+        return programRepository.findAll();
+    }
+
+    @Override
+    public Optional<Program> getProgramById(int id) {
+        return programRepository.findById(id);
+    }
 }
