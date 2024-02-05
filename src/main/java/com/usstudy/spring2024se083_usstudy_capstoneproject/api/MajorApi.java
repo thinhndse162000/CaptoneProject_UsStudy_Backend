cf5d1d094b2f58d935b341b217d47e5b25811a71
DESKTOP-AFRRDFT\Admin <thinhndse162000@fpt.edu.vn>
2024-02-05 20:59:40 +0700

    fix conflict

--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/api/MajorApi.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/api/MajorApi.java
@@ -1,14 +1,11 @@
 package com.usstudy.spring2024se083_usstudy_capstoneproject.api;
 
 import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.entity.Major;
-import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.service.interfaces.IMajorService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
