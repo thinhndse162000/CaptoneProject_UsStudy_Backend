8df84e195fadc5d93c4bdc9df588d84483306f1e
DESKTOP-AFRRDFT\Admin <thinhndse162000@fpt.edu.vn>
2024-03-10 17:55:38 +0700

    set cors access

--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/Spring2024Se083UsStudyCapstoneProjectApplication.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/Spring2024Se083UsStudyCapstoneProjectApplication.java
@@ -1,11 +1,18 @@
 package com.usstudy.spring2024se083_usstudy_capstoneproject;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://usstudy.monoinfinity.net", description = "Default Server URL")
+        }
+)
 @SpringBootApplication
 public class Spring2024Se083UsStudyCapstoneProjectApplication {
 
