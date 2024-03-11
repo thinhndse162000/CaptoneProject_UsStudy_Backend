43a734cd7daffb80f81c1a588a22f5c865a09f6c
DESKTOP-AFRRDFT\Admin <thinhndse162000@fpt.edu.vn>
2024-03-11 23:00:16 +0700

    Jwt authentication

--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/Spring2024Se083UsStudyCapstoneProjectApplication.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/Spring2024Se083UsStudyCapstoneProjectApplication.java
@@ -4,9 +4,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @OpenAPIDefinition(
         servers = {
@@ -20,18 +17,5 @@ public class Spring2024Se083UsStudyCapstoneProjectApplication {
         SpringApplication.run(Spring2024Se083UsStudyCapstoneProjectApplication.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://usstudy-be:8080", "https://usstudy.monoinfinity.net/swagger-ui/index.html", "http://localhost:3000")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowedOrigins("*")
-                        .maxAge(-1);
-            }
-        };
-    }
+
 }
