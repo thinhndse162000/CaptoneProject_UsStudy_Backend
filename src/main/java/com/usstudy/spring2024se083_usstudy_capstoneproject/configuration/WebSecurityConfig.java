119edbb6a5707f5e87dd473eff46b56f34ca3631
DESKTOP-AFRRDFT\Admin <thinhndse162000@fpt.edu.vn>
2024-03-14 20:04:05 +0700

    Jwt authentication

--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/configuration/WebSecurityConfig.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/configuration/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +19,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
 public class WebSecurityConfig implements WebMvcConfigurer {
 
 
