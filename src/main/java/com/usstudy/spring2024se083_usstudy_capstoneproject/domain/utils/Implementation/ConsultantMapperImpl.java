eec11c9170b12caf90e233a46e85ef9d6f491f23
DESKTOP-AFRRDFT\Admin <thinhndse162000@fpt.edu.vn>
2024-03-15 22:47:23 +0700

    get all account from 2 table

--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/utils/Implementation/ConsultantMapperImpl.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/utils/Implementation/ConsultantMapperImpl.java
@@ -5,13 +5,7 @@ import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.entity.Consult
 import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.utils.ConsultantMapper;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.processing.Generated;
 
-@Generated(
-        value = "org.mapstruct.ap.MappingProcessor",
-        date = "2023-03-24T20:50:35+0700",
-        comments = "version: 1.3.0.Final, compiler: javac, environment: Java 1.8.0_181 (Oracle Corporation)"
-)
 @Component
 public class ConsultantMapperImpl implements ConsultantMapper {
 
--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/utils/Implementation/CustomerMapperImpl.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/utils/Implementation/CustomerMapperImpl.java
@@ -5,14 +5,7 @@ import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.entity.Custome
 import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.utils.CustomerMapper;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.processing.Generated;
 
-
-@Generated(
-        value = "org.mapstruct.ap.MappingProcessor",
-        date = "2023-03-24T20:50:35+0700",
-        comments = "version: 1.3.0.Final, compiler: javac, environment: Java 1.8.0_181 (Oracle Corporation)"
-)
 @Component
 public class CustomerMapperImpl implements CustomerMapper {
     @Override
@@ -29,7 +22,7 @@ public class CustomerMapperImpl implements CustomerMapper {
         customerDto.setGender(customer.getGender());
         customerDto.setPassword(customer.getPassword());
         customerDto.setPhone(customer.getPhone());
-        customerDto.setCustomerId(customerDto.getCustomerId());
+        customerDto.setCustomerId(customer.getCustomerId());
         return customerDto;
     }
 }
