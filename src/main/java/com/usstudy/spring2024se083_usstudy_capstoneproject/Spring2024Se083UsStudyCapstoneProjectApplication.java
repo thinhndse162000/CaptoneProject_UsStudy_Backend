4c5ef9edb7456e642df4fbb449399e25acf90fd8
DESKTOP-AFRRDFT\Admin <thinhndse162000@fpt.edu.vn>
2024-03-20 11:46:18 +0700

    api customer get by id

--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/Spring2024Se083UsStudyCapstoneProjectApplication.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/Spring2024Se083UsStudyCapstoneProjectApplication.java
@@ -1,16 +1,18 @@
 package com.usstudy.spring2024se083_usstudy_capstoneproject;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@OpenAPIDefinition(
-//        servers = {
-//                @Server(url = "https://usstudy.monoinfinity.net", description = "Default Server URL")
-//        }
-//)
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://usstudy.monoinfinity.net", description = "Default Server URL")
+        }
+)
 @SpringBootApplication
 @SecurityScheme(name = "Authorization", scheme = "Bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class Spring2024Se083UsStudyCapstoneProjectApplication {
