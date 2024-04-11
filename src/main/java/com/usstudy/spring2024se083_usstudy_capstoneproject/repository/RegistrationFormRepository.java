4b62615fc15023b528e16cdf6ec511fc6bfc291e
Le Nguyen Hung <lnhungtn789@gmail.com>
2024-04-11 15:29:24 +0700

    get, create registration form

--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/repository/RegistrationFormRepository.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/repository/RegistrationFormRepository.java
@@ -1,16 +1,19 @@
 package com.usstudy.spring2024se083_usstudy_capstoneproject.repository;
 
 import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.entity.RegistrationForm;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface RegistrationFormRepository extends CrudRepository<RegistrationForm, Integer> {
+public interface RegistrationFormRepository extends JpaRepository<RegistrationForm, Integer> {
 
     @Query("select sp from RegistrationForm sp where sp.customer.customerId = :customerId")
-    Iterable<RegistrationForm> findByCustomerId(Integer customerId);
+    List<RegistrationForm> findByCustomerId(Integer customerId);
 
     @Query("select sp from RegistrationForm sp where sp.consultant.consultantId = :consultantId")
-    Iterable<RegistrationForm> findByConsultantId(Integer consultantId);
+    List<RegistrationForm> findByConsultantId(Integer consultantId);
 }
--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/service/implementation/RegistrationFormServiceImpl.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/service/implementation/RegistrationFormServiceImpl.java
@@ -2,29 +2,40 @@ package com.usstudy.spring2024se083_usstudy_capstoneproject.service.implementati
 
 import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.dto.request.RegistrationFormCreateRequest;
 import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.dto.request.RegistrationFormUpdateRequest;
+import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.dto.response.RegistrationFormDto;
 import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.entity.Consultant;
+import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.entity.Customer;
 import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.entity.RegistrationForm;
+import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.utils.RegistrationFormMapper;
 import com.usstudy.spring2024se083_usstudy_capstoneproject.repository.ConsultantRepository;
+import com.usstudy.spring2024se083_usstudy_capstoneproject.repository.CustomerRepository;
 import com.usstudy.spring2024se083_usstudy_capstoneproject.repository.RegistrationFormRepository;
 import com.usstudy.spring2024se083_usstudy_capstoneproject.service.RegistrationFormService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class RegistrationFormServiceImpl implements RegistrationFormService {
     private final RegistrationFormRepository registrationFormRepository;
     private final ConsultantRepository consultantRepository;
+    private final CustomerRepository customerRepository;
 
     @Autowired
-    public RegistrationFormServiceImpl(RegistrationFormRepository registrationFormRepository, ConsultantRepository consultantRepository) {
+    public RegistrationFormServiceImpl(RegistrationFormRepository registrationFormRepository, ConsultantRepository consultantRepository, CustomerRepository customerRepository) {
         this.registrationFormRepository = registrationFormRepository;
         this.consultantRepository = consultantRepository;
+        this.customerRepository = customerRepository;
     }
 
     @Override
     public void CreateRegistrationForm(RegistrationFormCreateRequest request) {
         RegistrationForm registrationForm = new RegistrationForm();
-
+        Customer customer=customerRepository.findById(request.getCustomerId()).orElseThrow(
+                ()->new NullPointerException("Customer not found - "+request.getCustomerId())
+        );
         registrationForm.setArea(request.getArea());
         registrationForm.setMoreInformation(request.getMoreInformation());
         registrationForm.setStudyAbroadReason(request.getStudyAbroadReason());
@@ -34,23 +45,29 @@ public class RegistrationFormServiceImpl implements RegistrationFormService {
         registrationForm.setUniversityChooseReason(request.getUniversityChooseReason());
         registrationForm.setPriorityOfStudyProgram(request.getPriorityOfStudyAbroad());
         registrationForm.setBudget(request.getBudget());
-
+        registrationForm.setCustomer(customer);
         registrationFormRepository.save(registrationForm);
     }
 
     @Override
-    public Iterable<RegistrationForm> getRegistrationFormByCustomer(Integer id) {
-        return registrationFormRepository.findByCustomerId(id);
+    public List<RegistrationFormDto> getRegistrationFormByCustomer(Integer id) {
+        return registrationFormRepository.findByCustomerId(id)
+                .stream().map(RegistrationFormMapper.INSTANCE::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Iterable<RegistrationForm> getRegistrationFormByConsultant(Integer id) {
-        return registrationFormRepository.findByConsultantId(id);
+    public List<RegistrationFormDto> getRegistrationFormByConsultant(Integer id) {
+        return registrationFormRepository.findByConsultantId(id)
+                .stream().map(RegistrationFormMapper.INSTANCE::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Iterable<RegistrationForm> getAll() {
-        return registrationFormRepository.findAll();
+    public List<RegistrationFormDto> getAll() {
+        return registrationFormRepository.findAll()
+                .stream().map(RegistrationFormMapper.INSTANCE::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
