204a99be3846c6a74e06a75ae82817d3f7cf3d88
DESKTOP-AFRRDFT\Admin <thinhndse162000@fpt.edu.vn>
2024-04-23 14:24:52 +0700

    hot fix

--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/dto/request/ConsultantRequest.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/dto/request/ConsultantRequest.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -15,6 +17,6 @@ public class ConsultantRequest {
     private String password;
     private String introduction;
     private String education;
-    private String specialize;
+    private List<String> specialize;
     private String img;
 }
--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/utils/ConsultantMapper.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/utils/ConsultantMapper.java
@@ -5,13 +5,24 @@ import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.dto.response.C
 import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.entity.Consultant;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper
 public interface ConsultantMapper {
     ConsultantMapper INSTANT = Mappers.getMapper(ConsultantMapper.class);
-    @Mapping(target = "userName",source = "username")
+
+    @Mapping(target = "userName", source = "username")
     ConsultantDto consultantToConsultantDto(Consultant consultant);
-    @Mapping(target = "userName",source = "userName")
+
+    @Mapping(target = "userName", source = "userName")
+    @Mapping(target = "specialize", source = "specialize", qualifiedByName = "convertListToString")
     Consultant toEntity(ConsultantRequest consultantRequest);
+
+    @Named("convertListToString")
+    default String convertListToString(List<String> list) {
+        return list != null ? String.join(",", list) : null;
+    }
 }
