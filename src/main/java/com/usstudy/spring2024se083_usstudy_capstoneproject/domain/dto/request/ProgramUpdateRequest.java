c3bf34285522446289e93a0191303078973a936c
Le Nguyen Hung <lnhungtn789@gmail.com>
2024-04-25 15:37:08 +0700

    Update ProgramUpdateRequest.java

--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/dto/request/ProgramUpdateRequest.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/dto/request/ProgramUpdateRequest.java
@@ -14,6 +14,7 @@ import java.sql.Date;
 public class ProgramUpdateRequest {
     
     private String nameProgram;
+    private String status;
     private Date createDate;
     private Date modifiedDate;
     private Integer modifier;
