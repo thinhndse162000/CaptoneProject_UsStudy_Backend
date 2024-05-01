4cf80b33ac3b3294e29d781c51dff6f2cd6eb751
Le Nguyen Hung <lnhungtn789@gmail.com>
2024-05-01 15:31:00 +0700

    Update PaymentDto.java

--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/dto/response/PaymentDto.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/domain/dto/response/PaymentDto.java
@@ -12,6 +12,7 @@ import java.sql.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PaymentDto {
+    private Integer paymentId;
     private Integer programApplicationId;
     private String method;
     private int amount;
