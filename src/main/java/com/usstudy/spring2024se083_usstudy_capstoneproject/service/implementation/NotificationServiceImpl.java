5bedbbc4adc7f524b6ad1ffbfa17eb122628dcd5
DESKTOP-AFRRDFT\Admin <thinhndse162000@fpt.edu.vn>
2024-05-02 10:41:20 +0700

    hot fix

--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/service/implementation/NotificationServiceImpl.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/service/implementation/NotificationServiceImpl.java
@@ -75,13 +75,7 @@ public class NotificationServiceImpl implements NotificationService {
             notification.setDate(new Date(System.currentTimeMillis()));
             notification.setCustomer(customer);
             notification.setTitle("Cập nhập trạng thái đơn tư vấn");
-            if (registrationForm.getStatus() == 0) {
-                notification.setContent("Tư vấn viên " + " đã cập nhập trạng thái đơn tư vấn thành " + "Chưa được tư vấn");
-            } else if (registrationForm.getStatus() == 1) {
-                notification.setContent("Tư vấn viên " + " đã cập nhập trạng thái đơn tư vấn thành " + "Đang được tư vấn " + "Xin hãy chờ tư vấn viên sẽ liên lạc cho bạn");
-            } else if (registrationForm.getStatus() == 2) {
-                notification.setContent("Tư vấn viên " + " đã cập nhập trạng thái đơn tư vấn thành " + "Đã được tư vấn");
-            }
+            notification.setContent("Tư Vấn trạng thái đơn tư vấn" + registrationForm.getRegistrationFormId());
             notificationRepository.save(notification);
         } else if (request.getProgramId() != null && request.getApplyStageId() != null) {
             Program program = programRepository.findById(request.getProgramId())
