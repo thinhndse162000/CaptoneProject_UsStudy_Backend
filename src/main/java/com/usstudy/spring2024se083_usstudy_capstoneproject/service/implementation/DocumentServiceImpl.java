0017af6ad0d98e1bbda6e5539ab2be3340a6b431
Le Nguyen Hung <lnhungtn789@gmail.com>
2024-05-31 19:55:42 +0700

    Update DocumentServiceImpl.java

--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/service/implementation/DocumentServiceImpl.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/service/implementation/DocumentServiceImpl.java
@@ -49,7 +49,7 @@ public class DocumentServiceImpl implements DocumentService {
         }
         Document document= DocumentMapper.INSTANCE.toEntity(documentRequest);
         document.setUpdateDate(new Date(System.currentTimeMillis()));
-        document.setStatus(1);
+        document.setStatus(0);
         return DocumentMapper.INSTANCE.toDto(
                 documentRepository.save(document)
         );
