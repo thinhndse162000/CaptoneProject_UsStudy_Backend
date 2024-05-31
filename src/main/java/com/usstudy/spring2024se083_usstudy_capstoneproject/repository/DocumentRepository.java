f352b49f8e98a51f847c60cf1031407997cf8091
Le Nguyen Hung <lnhungtn789@gmail.com>
2024-05-31 20:14:35 +0700

    Update DocumentRepository.java

--- src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/repository/DocumentRepository.java
+++ src/main/java/com/usstudy/spring2024se083_usstudy_capstoneproject/repository/DocumentRepository.java
@@ -2,11 +2,15 @@ package com.usstudy.spring2024se083_usstudy_capstoneproject.repository;
 
 import com.usstudy.spring2024se083_usstudy_capstoneproject.domain.entity.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface DocumentRepository extends JpaRepository<Document,Integer> {
+    @Query("Select d from Document d " +
+            "WHERE d.programApplication.programApplicationId = :id " +
+            "ORDER BY d.updateDate DESC")
     List<Document> getAllByProgramApplicationProgramApplicationId(Integer id);
 }
